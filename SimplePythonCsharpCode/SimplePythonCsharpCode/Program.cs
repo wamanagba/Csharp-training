47400a29993580b21f8ab4a8c85f162f82968beb
Yacouba <yacouba.ouedraogo@aims-senegal.org>
2023-05-04 16:46:05 +0000

    R and Python  code in C#

---
+++ SimplePythonCsharpCode/SimplePythonCsharpCode/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using IronPython.Hosting;
+using System.IO;
+
+namespace SimplePythonCsharpCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instance of python engine
+            var engine = Python.CreateEngine();
+            //reading code from file
+            var source = engine.CreateScriptSourceFromFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "simple.py"));
+            var scope = engine.CreateScope();
+            //executing script in scope
+            source.Execute(scope);
+            var classCalculator = scope.GetVariable("calculator");
+            //initializing class
+            var calculatorInstance = engine.Operations.CreateInstance(classCalculator);
+            Console.WriteLine("From Iron Python");
+            Console.WriteLine("5 + 10 = {0}", calculatorInstance.sum(5, 10));
+            Console.WriteLine("5 -10 = {0}", calculatorInstance.sous(5,10));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SimpleRscript_Csharp/SimpleRscript_Csharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using RDotNet;
+using System;
+using System.IO;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // R session initialization 
+        REngine.SetEnvironmentVariables();
+        REngine engine = REngine.GetInstance();
+
+        // Read R code from a file
+        string scriptPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "script.R");
+        string script = File.ReadAllText(scriptPath);
+
+        // Running the R code
+        engine.Evaluate(script);
+
+        // Call the 'mean' function from R code named Calcul
+        NumericVector vector = engine.Evaluate("Calcul(c(1, 2, 3, 4, 5))").AsNumeric();
+
+        // Recovery of return value
+        double mean = vector[0];
+
+        // Display of calculated average
+        Console.WriteLine("La moyenne est : " + mean);
+        Console.ReadLine();
+        // Cleaning the R session
+        engine.Dispose();
+    }
+}
