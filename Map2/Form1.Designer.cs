7636e8f9c6c9b1e780f17ac8f0c816385f6c6700
Yacouba <yacouba.ouedraogo@aims-senegal.org>
2023-06-05 02:13:49 +0000

    Mapp02

---
+++ Map2/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿
+namespace Map2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerateMap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGenerateMap
+            // 
+            this.btnGenerateMap.Location = new System.Drawing.Point(775, 23);
+            this.btnGenerateMap.Name = "btnGenerateMap";
+            this.btnGenerateMap.Size = new System.Drawing.Size(242, 60);
+            this.btnGenerateMap.TabIndex = 0;
+            this.btnGenerateMap.Text = "Generate Map";
+            this.btnGenerateMap.UseVisualStyleBackColor = true;
+            this.btnGenerateMap.Click += new System.EventHandler(this.btnGenerateMap_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1029, 622);
+            this.Controls.Add(this.btnGenerateMap);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerateMap;
+    }
+}
+
---
+++ Map2/Form1.cs
@@ -0,0 +1,99 @@
+﻿
+
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using RDotNet;
+using System.IO;
+
+namespace Map2
+{
+    public partial class Form1 : Form
+    {
+        private REngine engine;
+        private PictureBox pictureBox;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+
+
+      
+
+        private void btnGenerateMap_Click(object sender, EventArgs e)
+        {
+            //Specifies the path to the R binary
+            REngine.SetEnvironmentVariables("C:/Program Files/R/R-3.6.0/bin/i386", "C:/Program Files/R/R-3.6.0");
+
+            // R session initialization 
+            REngine engine = REngine.GetInstance();
+
+            // Read R code from a file
+            string scriptPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "scripts.R");
+            string script = File.ReadAllText(scriptPath);
+            // Running the R code
+            engine.Evaluate(script);
+
+            MessageBox.Show("Download Succefully");
+
+            // Récupérer la carte générée en tant qu'image et créer le contrôle PictureBox
+            string imagePath = System.IO.Path.Combine(Application.StartupPath, "Percentage_Aug_2022.jpeg");
+            Bitmap mapImage = new Bitmap(imagePath);
+
+            // Créer le contrôle PictureBox
+            pictureBox = new PictureBox
+            {
+                Image = mapImage,
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Location = new Point(10, 10),
+                Size = new Size(200, 300)
+            };
+
+            pictureBox2 = new PictureBox
+            {
+                Image = mapImage,
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Location = new Point(200, 10),
+                Size = new Size(200, 300)
+            };
+
+            pictureBox3 = new PictureBox
+            {
+                Image = mapImage,
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Location = new Point(400, 10),
+                Size = new Size(200, 300)
+            };
+
+            pictureBox4 = new PictureBox
+            {
+                Image = mapImage,
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Location = new Point(10, 320),
+                Size = new Size(200, 300)
+            };
+
+            Controls.Add(pictureBox);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox4);
+        }
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Fermer l'environnement R.NET
+            engine.Dispose();
+        }
+    }
+}
